9befa081448c1549f2e5e3fe3dedb99f9e2e63f9
Björn <Bjorn.oudkerk@outlook.com>
2022-05-24 14:49:34 +0200

    Initial commit

---
+++ Assets/Scripts/Coral/CoralPlacer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+
+public class CoralPlacer : MonoBehaviour
+{
+    [SerializeField] private Transform raycastPos;
+    private ARRaycastManager _raycastManager;
+    private bool _grounded;
+    private void Awake()
+    {
+        _raycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    private void Update()
+    {
+        if (_raycastManager != null && !_grounded)
+        {
+            FindGround();
+        }
+    }
+
+    private void FindGround()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(raycastPos.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+        {
+            transform.position = hit.transform.position;
+            _grounded = true;
+        }
+    }
+}
