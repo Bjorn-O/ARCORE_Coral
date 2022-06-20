09052ac8740e877191e1d432c2dc447d8935549d
Zakaria-ElGhoul <30044@ma-web.nl>
2022-06-20 11:20:27 +0200

    Added a way to change material properties in real time

--- Assets/Scripts/Coral/CoralReef.cs
+++ Assets/Scripts/Coral/CoralReef.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CoralReef : MonoBehaviour
 {
+    [SerializeField] float health;
+    public Component[] components;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleSaturation();
+    }
+
+    void HandleSaturation()
+    {
+        components = GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer mesh in components)
+        {
+            mesh.material.SetFloat("_Saturation_Amount", health / 100);
+        }
     }
 }
